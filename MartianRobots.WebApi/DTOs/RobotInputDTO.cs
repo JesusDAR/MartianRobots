6fca10abdf7e3fe5a40042edd61c7289cb21a18e
Jesus <jesus.rodrigo@samelan.com>
2022-01-10 21:45:51 +0100

    DTOs

---
+++ MartianRobots.WebApi/DTOs/RobotInputDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MartianRobots.WebApi.DTOs
+{
+    public class RobotInputDTO
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Or { get; set; }
+        public string Moves { get; set; }
+    }
+}
