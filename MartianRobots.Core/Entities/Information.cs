62d8d2db108abd7214148f81478eb33e10c5fbcb
Jesus <jesus.rodrigo@samelan.com>
2022-01-12 11:12:00 +0100

    Entities

---
+++ MartianRobots.Core/Entities/Information.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartianRobots.Core.Entities
+{
+    public class Information
+    {
+        public int Id { get; set; }
+        public int RobotsSucceeded { get; set; }
+        public int RobotsLost { get; set; }
+    }
+}
