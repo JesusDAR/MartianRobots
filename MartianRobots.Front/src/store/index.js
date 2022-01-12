37a832553bd8e4d6275bc20776428bd60c4f464a
Jesus <jesus.rodrigo@samelan.com>
2022-01-12 21:42:53 +0100

    front

---
+++ MartianRobots.Front/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    mars: {
+      x: 0,
+      y: 0
+    }
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ MartianRobots.Front/src/views/Layout.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <table>
+                    <tr>
+                        <td>
+                            aaaa
+                        </td>
+                        <td>
+                            aaaa
+                        </td>
+                        <td>
+                            aaaa
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            aaaa
+                        </td>
+                        <td>
+                            aaaa
+                        </td>
+                        <td>
+                            aaaa
+                        </td>
+                    </tr>                    
+                </table>
+            </v-row>
+        </v-container>
+    </div>
+  
+  </v-row>
+</template>
+
+<script>
+export default {
+    name: "Layout"
+}
+</script>
+
+<style>
+
+table, th, td {
+  border:  solid black;
+  border-collapse: collapse;
+}
+</style>
